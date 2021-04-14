ea8d2b9445fd199c521afa858d70ed59f19a9017
Victor Jimenez <betabandido@gmail.com>
2021-04-14 19:15:17 +0200

    Add output for OIDC identity provider issuer

--- output.tf
+++ output.tf
@@ -5,3 +5,7 @@ output "worker_role_arn" {
 output "private_subnet_ids" {
   value = data.aws_subnet_ids.private.ids
 }
+
+output "oidc_identity_provider_issuer" {
+  value = aws_eks_cluster.cluster.identity[0].oidc[0].issuer
+}
