d547aeb02d01846e0d27332127f6d36471ee2a6b
xorxsan <jorge.sanchezaparicio@vistaprint.com>
2021-07-22 15:15:14 +0200

    Code review comments

--- variables.tf
+++ variables.tf
@@ -40,7 +40,7 @@ variable "cluster_log_types" {
   default    = ["api", "authenticator"]
 
   validation {
-    condition      = length(setintersection(var.cluster_log_types, ["api", "audit", "authenticator", "controllerManager", "scheduler"])) > 0
+    condition      = length(setsubtract(var.cluster_log_types, ["api", "audit", "authenticator", "controllerManager", "scheduler"])) == 0
     error_message  = "Control plane logs can only be a list containing any of api, audit, authenticator, controllerManager or scheduler."
   }
 }
