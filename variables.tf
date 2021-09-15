34a615b8b65967fe5981b93af1eadccbad711f54
xorxsan <36991286+xorxsan@users.noreply.github.com>
2021-09-15 11:12:58 +0200

    Allow for more control plane logs (#9)
    * Enable all control plane logs

--- variables.tf
+++ variables.tf
@@ -34,3 +34,13 @@ variable "use_calico_cni" {
   type    = bool
   default = false
 }
+
+variable "cluster_log_types" {
+  type       = list(string)
+  default    = ["api", "authenticator"]
+
+  validation {
+    condition      = length(setsubtract(var.cluster_log_types, ["api", "audit", "authenticator", "controllerManager", "scheduler"])) == 0
+    error_message  = "Control plane logs can only be a list containing any of api, audit, authenticator, controllerManager or scheduler."
+  }
+}
