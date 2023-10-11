5f2753e929b80a032a546426afe1b0a96631fc54
Victor Jimenez <vjimenez@vista.com>
2023-10-11 13:41:38 +0200

    Update test cluster version

--- examples/basic/cluster.tf
+++ examples/basic/cluster.tf
@@ -2,7 +2,7 @@ module "cluster" {
   source  = "./../.."
 
   cluster_name      = "simple-eks-integration-test"
-  cluster_version   = "1.23"
+  cluster_version   = "1.28"
   vpc_name          = var.vpc_name
   cluster_log_types = ["api", "audit", "authenticator"]
 
