6e7053889aba2d33559cd7c847632f248d26fbe4
Victor Jimenez <vjimenez@vista.com>
2025-08-11 19:43:02 +0200

    Remove unnecessary outputs

--- examples/basic/outputs.tf
+++ examples/basic/outputs.tf
@@ -1,11 +1,3 @@
-# output "worker_role_arn" {
-#   value = module.cluster.worker_role_arn
-# }
-
-# output "private_subnet_ids" {
-#   value = module.cluster.private_subnet_ids
-# }
-
 output "oidc_identity_provider_issuer" {
   value = module.cluster.oidc_identity_provider_issuer
 }
